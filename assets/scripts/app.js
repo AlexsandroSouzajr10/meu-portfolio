808069cc757be17e1d86fbb8b5db5e7d6bb513ec
AlexsandroSouzajr <alexsandrosouzajr10@gmail.com>
2024-06-29 23:09:17 -0300

    updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,8 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
+    if (this.window.innerWidth <= 550) {
+        header.classList.toggle("sticky", window.scrollY > 0);
+    }
+});
 
 AOS.init();
